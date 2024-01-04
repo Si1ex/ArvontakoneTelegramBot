e1d1b4ffe9ba8407e4c6b80da5ad6d9705b08981
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2024-01-04 13:10:19 +0200

    Changed pythonExecutable

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const pythonBotScript = 'main.py';
+const pythonExecutable = '/c/Python310/python';
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 
-  const pythonBotProcess = exec(`python ${pythonBotScript}`);
+  const pythonBotProcess = exec(`${pythonExecutable} ${pythonBotScript}`);
 
   pythonBotProcess.stdout.on('data', (data) => {
     console.log(`Bot stdout: ${data}`);
   });
-  
+
   pythonBotProcess.stderr.on('data', (data) => {
     console.error(`Bot stderr: ${data}`);
   });
-  
+
   pythonBotProcess.on('close', (code) => {
     console.log(`Bot process exited with code ${code}`);
   });
