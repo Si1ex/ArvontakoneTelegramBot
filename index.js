0b2749ec0420c8530991cc1ed7828c1149d7870c
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2024-01-04 13:23:13 +0200

    removed cross_env

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { exec } = require('child_process');
-const crossEnv = require('cross-env');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const pythonBotScript = 'main.py';
-const pythonExecutable = 'C:\\Python310\\python.exe'; 
+const pythonExecutable = 'C:\\Python310\\python.exe';
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 
-  const pythonBotProcess = exec(crossEnv(`"${pythonExecutable}" ${pythonBotScript}`));
+  const pythonBotProcess = exec(`${pythonExecutable} ${pythonBotScript}`);
 
   pythonBotProcess.stdout.on('data', (data) => {
     console.log(`Bot stdout: ${data}`);
